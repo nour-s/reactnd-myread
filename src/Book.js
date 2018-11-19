d1a7db209b65b9289941f8fa725ef4ee77111a22
nour-s <programmer.nour@gmail.com>
2018-11-19 22:46:34 +0000

    RNDMR-4 Fix the context menu in Search to show the correct shelf

--- src/Book.js
+++ src/Book.js
@@ -10,7 +10,7 @@ class Book extends React.Component
                             <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}>
 							</div>
                             <div className="book-shelf-changer">
-                              <select>
+                              <select value={book.shelf || "none"}>
                                 <option value="move" disabled>Move to...</option>
                                 <option value="currentlyReading">Currently Reading</option>
                                 <option value="wantToRead">Want to Read</option>
