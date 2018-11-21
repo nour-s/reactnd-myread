c6f3e0964e03be349cb1265d2fdf9da903c5bdec
Nour Sabouny <programmer.nour@gmail.com>
2018-11-21 21:04:45 +0400

    RNDMR-6 Clear search when going back from search page.

--- src/SearchBooks.js
+++ src/SearchBooks.js
@@ -3,15 +3,19 @@ import { Link } from "react-router-dom";
 import BookCase from "./BookCase";
 
 class SearchBooks extends React.Component {
-  render() {
-    return (
-      <div className="search-books">
-        <div className="search-books-bar">
-          <Link className="close-search" to={{ pathname: "/" }}>
-            Close{" "}
-          </Link>
-          <div className="search-books-input-wrapper">
-            {/*
+	componentWillUnmount() {
+		this.props.onClearSearch();
+	}
+
+	render() {
+		return (
+			<div className="search-books">
+				<div className="search-books-bar">
+					<Link className="close-search" to={{ pathname: "/" }}>
+						Close{" "}
+					</Link>
+					<div className="search-books-input-wrapper">
+						{/*
                   NOTES: The search from BooksAPI is limited to a particular set of search terms.
                   You can find these search terms here:
                   https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
@@ -19,25 +23,25 @@ class SearchBooks extends React.Component {
                   However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
                   you don't find a specific author or title. Every search is limited by search terms.
                 */}
-            <input
-              type="text"
-              onChange={this.props.onSearch}
-              placeholder="Search by title or author"
-            />
-          </div>
-        </div>
-        <div className="search-books-results">
-          {this.props.shelfs.length > 0 && (
-            <BookCase
-              shelfs={this.props.shelfs}
-              onMoveBook={this.props.onMoveBook}
-            />
-          )}
-          <ol className="books-grid" />
-        </div>
-      </div>
-    );
-  }
+						<input
+							type="text"
+							onChange={this.props.onSearch}
+							placeholder="Search by title or author"
+						/>
+					</div>
+				</div>
+				<div className="search-books-results">
+					{this.props.shelfs.length > 0 && (
+						<BookCase
+							shelfs={this.props.shelfs}
+							onMoveBook={this.props.onMoveBook}
+						/>
+					)}
+					<ol className="books-grid" />
+				</div>
+			</div>
+		);
+	}
 }
 
 export default SearchBooks;
