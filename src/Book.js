d4b21418966a335a2adb3bf4c293935a8281f35e
Nour Sabouny <programmer.nour@gmail.com>
2018-11-22 02:42:26 +0400

    RNDMOR-6 Add type check using PropTypes for all components

--- src/Book.js
+++ src/Book.js
@@ -1,47 +1,65 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 class Book extends React.Component {
-  onShelfChanged = select => {
-    var shelfId = select.target.value;
-    this.props.onMoveBook(this.props.book, shelfId);
-  };
+	onShelfChanged = select => {
+		var shelfId = select.target.value;
+		this.props.onMoveBook(this.props.book, shelfId);
+	};
 
-  render() {
-    var book = this.props.book;
-    return (
-      <div className="book">
-        <div className="book-top">
-          <div
-            className="book-cover"
-            style={{
-              width: 128,
-              height: 193,
-              backgroundImage: `url(${
-                (book.imageLinks || { thumbnail: "" }).thumbnail
-              })`
-            }}
-          />
-          <div className="book-shelf-changer">
-            <select value={book.shelf || "none"} onChange={this.onShelfChanged}>
-              <option value="move" disabled>
-                Move to...
-              </option>
-              <option value="currentlyReading">Currently Reading</option>
-              <option value="wantToRead">Want to Read</option>
-              <option value="read">Read</option>
-              <option value="none">None</option>
-            </select>
-          </div>
-        </div>
-        <div className="book-title">{book.title}</div>
-        {book.authors && (
-          <div className="book-authors">
-            {book.authors.reduce((all, author) => `${all} - ${author}`)}
-          </div>
-        )}
-      </div>
-    );
-  }
+	render() {
+		var book = this.props.book;
+		return (
+			<div className="book">
+				<div className="book-top">
+					<div
+						className="book-cover"
+						style={{
+							width: 128,
+							height: 193,
+							backgroundImage: `url(${
+								(book.imageLinks || { thumbnail: "" }).thumbnail
+							})`
+						}}
+					/>
+					<div className="book-shelf-changer">
+						<select
+							value={book.shelf || "none"}
+							onChange={this.onShelfChanged}
+						>
+							<option value="move" disabled>
+								Move to...
+							</option>
+							<option value="currentlyReading">
+								Currently Reading
+							</option>
+							<option value="wantToRead">Want to Read</option>
+							<option value="read">Read</option>
+							<option value="none">None</option>
+						</select>
+					</div>
+				</div>
+				<div className="book-title">{book.title}</div>
+				{book.authors && (
+					<div className="book-authors">
+						{book.authors.reduce(
+							(all, author) => `${all} - ${author}`
+						)}
+					</div>
+				)}
+			</div>
+		);
+	}
 }
 
+Book.propTypes = {
+	book: PropTypes.shape({
+		title: PropTypes.string,
+		shelf: PropTypes.string,
+		authors: PropTypes.array,
+		imageLinks: PropTypes.shape({
+			thumbnail: PropTypes.string
+		})
+	})
+};
 export default Book;
--- src/BookCase.js
+++ src/BookCase.js
@@ -1,31 +1,36 @@
 import React from "react";
+import PropTypes from "prop-types";
 import Shelf from "./Shelf";
 import { Link } from "react-router-dom";
 
 class BookCase extends React.Component {
-  render() {
-    return (
-      <div className="list-books">
-        <div className="list-books-title">
-          <h1>MyReads</h1>
-        </div>
-        <div className="list-books-content">
-          <div>
-            {this.props.shelfs.map(shelf => (
-              <Shelf
-                onMoveBook={this.props.onMoveBook}
-                key={shelf.id}
-                shelf={shelf}
-              />
-            ))}
-          </div>
-        </div>
-        <div className="open-search">
-          <Link to={{ pathname: "/search" }}>Add a book</Link>
-        </div>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="list-books">
+				<div className="list-books-title">
+					<h1>MyReads</h1>
+				</div>
+				<div className="list-books-content">
+					<div>
+						{this.props.shelfs.map(shelf => (
+							<Shelf
+								onMoveBook={this.props.onMoveBook}
+								key={shelf.id}
+								shelf={shelf}
+							/>
+						))}
+					</div>
+				</div>
+				<div className="open-search">
+					<Link to={{ pathname: "/search" }}>Add a book</Link>
+				</div>
+			</div>
+		);
+	}
 }
+
+BookCase.propTypes = {
+	shelfs: PropTypes.array
+};
 
 export default BookCase;
