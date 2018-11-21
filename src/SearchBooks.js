a21c2a5ec321e093df7ef7a640e92082836e32af
Nour Sabouny <programmer.nour@gmail.com>
2018-11-21 19:51:08 +0400

    dev/format-code Run prttier on all files

--- src/SearchBooks.js
+++ src/SearchBooks.js
@@ -1,16 +1,17 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import BookCase from './BookCase'
+import React from "react";
+import { Link } from "react-router-dom";
+import BookCase from "./BookCase";
 
-class SearchBooks extends React.Component
-{
-  render(){
+class SearchBooks extends React.Component {
+  render() {
     return (
-       <div className="search-books">
-            <div className="search-books-bar">
-      		<Link className="close-search" to={{ pathname:"/" }}>Close </Link>
-              <div className="search-books-input-wrapper">
-                {/*
+      <div className="search-books">
+        <div className="search-books-bar">
+          <Link className="close-search" to={{ pathname: "/" }}>
+            Close{" "}
+          </Link>
+          <div className="search-books-input-wrapper">
+            {/*
                   NOTES: The search from BooksAPI is limited to a particular set of search terms.
                   You can find these search terms here:
                   https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
@@ -18,17 +19,24 @@ class SearchBooks extends React.Component
                   However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
                   you don't find a specific author or title. Every search is limited by search terms.
                 */}
-                <input type="text" onChange={this.props.onSearch} placeholder="Search by title or author"/>
-
-              </div>
-            </div>
-            <div className="search-books-results">
-			{this.props.shelfs.length > 0 && <BookCase shelfs={this.props.shelfs} onMoveBook={this.props.onMoveBook} />}
-              <ol className="books-grid">
-			  </ol>
-            </div>
+            <input
+              type="text"
+              onChange={this.props.onSearch}
+              placeholder="Search by title or author"
+            />
           </div>
-      );
+        </div>
+        <div className="search-books-results">
+          {this.props.shelfs.length > 0 && (
+            <BookCase
+              shelfs={this.props.shelfs}
+              onMoveBook={this.props.onMoveBook}
+            />
+          )}
+          <ol className="books-grid" />
+        </div>
+      </div>
+    );
   }
 }
 
--- src/Shelf.js
+++ src/Shelf.js
@@ -1,20 +1,23 @@
-import React from 'react'
-import Book from './Book'
+import React from "react";
+import Book from "./Book";
 
-class Shelf extends React.Component
-{
-	render(){
-     	return (
-				<div className="bookshelf">
-                  <h2 className="bookshelf-title">{this.props.shelf.name}</h2>
-                  <div className="bookshelf-books">
-                    <ol className="books-grid">
-			          {this.props.shelf.books.map(book => (<li key={book.id}><Book book={book} onMoveBook={this.props.onMoveBook} /></li>))}
-                    </ol>
-                  </div>
-                </div>
-        );
-    }
+class Shelf extends React.Component {
+  render() {
+    return (
+      <div className="bookshelf">
+        <h2 className="bookshelf-title">{this.props.shelf.name}</h2>
+        <div className="bookshelf-books">
+          <ol className="books-grid">
+            {this.props.shelf.books.map(book => (
+              <li key={book.id}>
+                <Book book={book} onMoveBook={this.props.onMoveBook} />
+              </li>
+            ))}
+          </ol>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Shelf;
